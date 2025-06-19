d7099d2436462b5cb893133dbe4ade714969058f
joshceo213 <joshuamiti41@gmail.com>
2025-06-19 22:16:42 +0200

    Delete routes/DefaultRoute.js

--- routes/DefaultRoute.js
+++
@@ -1,8 +0,0 @@
-app.get('/', (req, res) => {
-  res.send('Zipper Backend is running 🚐');
-});
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
