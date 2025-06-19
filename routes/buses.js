9605b6079d3a8653dd0c51235f0b91538c9be7a3
joshceo213 <joshuamiti41@gmail.com>
2025-06-19 22:11:33 +0200

    Update buses.js

--- routes/buses.js
+++ routes/buses.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
-// Mock data
+// Mock buses for now
 const buses = [
-  { id: 1, operator: 'Power Tools', from: 'Lusaka', to: 'Kitwe', time: '08:00', price: 250 },
-  { id: 2, operator: 'Juldan Motors', from: 'Ndola', to: 'Lusaka', time: '09:30', price: 230 },
+  { id: 1, name: 'UBZ', route: 'Lusaka to Ndola', price: 200 },
+  { id: 2, name: 'Power Tools', route: 'Lusaka to Kitwe', price: 250 },
+  { id: 3, name: 'Jonda', route: 'Lusaka to Livingstone', price: 280 },
+  { id: 4, name: 'Juldan Motors', route: 'Lusaka to Chipata', price: 230 },
+  { id: 5, name: 'Shalom', route: 'Lusaka to Mongu', price: 300 },
+  { id: 6, name: 'Likili', route: 'Lusaka to Solwezi', price: 290 },
+  { id: 7, name: 'Sam Logix', route: 'Lusaka to Kasama', price: 310 },
 ];
 
-// GET all buses
 router.get('/', (req, res) => {
   res.json(buses);
 });
